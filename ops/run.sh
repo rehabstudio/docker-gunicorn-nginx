6eafdfe5900cf0a9ec27c88de6a27d31ce1f113c
Peter McConnell <pemcconnell@googlemail.com>
2014-08-01 09:57:44 +0100

    Added check to make sure the folder actually exists

--- ops/run.sh
+++ ops/run.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# make sure the environment variable 'ENV' is set
 if [ -z "$ENV" ]
 then
     echo "!!! You must set an ENV variable !!!"
     echo "docker run -P -d -e ENV=dev <yourboxname>"
+    exit
+fi
+
+# make sure the environment variable 'ENV' matches a folder
+if [ ! -d "./$ENV" ]
+then
+    echo "!!! Your ENV variable must match a folder name in ./ops/ !!!"
+    echo "docker run -P -d -e ENV=<foldername> <yourboxname>"
     exit
 fi
 
