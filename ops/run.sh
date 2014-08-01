ffc52ef1762b996c0ed70d317fd8fb589b77e96e
Peter McConnell <pemcconnell@googlemail.com>
2014-08-01 09:13:56 +0100

    Added simple check for ensuring the environment variable is set

--- ops/run.sh
+++ ops/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+if [ ! -n "$ENV" ]
+then
+    echo "!!! You must set an ENV variable !!!"
+    echo "docker run -P -d -e ENV=dev <yourboxname>"
+    exit
+fi
+
 ./$ENV/docker-build.sh
 ./$ENV/docker-run.sh
