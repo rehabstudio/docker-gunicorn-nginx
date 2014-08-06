de222cc5f2b937638aeda7262136ce3e5d2f7afe
Peter McConnell <pemcconnell@googlemail.com>
2014-08-06 16:34:47 +0100

    Changed var casing

--- ops/live/docker-build.sh
+++ ops/live/docker-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-appPath=/var/app
+APP_PATH=/var/app
 
 # installs
 apt-get install -y nginx supervisor
 
 # pip
-pipPath=$appPath/requirements.txt
+pipPath=$APP_PATH/requirements.txt
 if [ -f $pipPath ];
 then
     pip install -r $pipPath
@@ -17,5 +17,5 @@ ln -sf /ops/live/supervisord.conf /etc/supervisord.conf
 
 # nginx
 ln -sf /ops/live/vhost.conf /etc/nginx/sites-enabled/default
-chown -R www-data $appPath
+chown -R www-data $APP_PATH
 service nginx restart
